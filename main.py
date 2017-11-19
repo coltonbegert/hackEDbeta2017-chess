08cae490398109e195186758770f482fa307beb4
Colton Begert <begert@ualberta.ca>
2017-11-19 02:43:10 -0700

    making main game logic

--- main.py
+++ main.py
@@ -7,11 +7,25 @@ def get_state():
 def update():
     pass
 
-def find_attacks(piece):
-    pass
+def find_attacks(board, filei, ranki):
+    return board.attacks(chess.square(filei, ranki))
 
-def play_move():
-    pass
+def get_moves(board, source):
+    moves = []
+    for move in board.legal_moves:
+        # move = chess.Move()
+        if move.from_square == source:
+            moves.append(move.to_square)
+    return moves
+
+def play_move(board, move):
+    board.push(move)
+    return
+
+def move(board, source, dest):
+    for move in board.legal_moves:
+        if move.from_square == source and move.to_square == dest:
+            return move
 
 board = chess.Board()
 mf_io = MidiFighterIO()
@@ -21,12 +35,19 @@ while not board.is_game_over():
     print(board)
     mf_io.send_board_state(board)
 
-    next_square = mf_io.get_square()
-    board_piece = board.piece_at(chess.square(next_square))
+    # next_square = mf_io.get_square()
+    file_index, rank_index = mf_io.get_square()
+    square = chess.square(file_index, rank_index)
+    # color = board.piece_at(square).color
+    board_piece = board.piece_at(square)
     if board_piece.color == board.turn:
-        possible_attacks = find_attacks(board_piece)
+        # possible_attacks = find_attacks(board_piece)
+        # for i in get_moves(board, square):
+        possible_attacks = get_moves(board, square)
+            
         mf_io.send_piece_selected(next_piece['coords'], possible_attacks)
 
-        next_attack = mf_io.get_square()
+        next_file, next_rank = mf_io.get_square()
+        next_attack = chess.square(next_file,next_rank)
         if next_attack in possible_attacks:
-            play_move(next_attack)
+            play_move(board, move(board, square, next_attack))

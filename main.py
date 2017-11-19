70710b75642428e6e14cf31b08a96a89711f6cd2
Colton Begert <begert@ualberta.ca>
2017-11-19 00:41:17 -0700

    lets save a file before we commit it

---
+++ main.py
@@ -0,0 +1,22 @@
+import chess
+
+def get_state()
+    pass
+
+def update()
+    pass
+
+def find_attacks()
+    pass
+
+def play_move()
+    pass
+
+board = chess.Board()
+
+while not board.is_game_over():
+    # This is our main game loop
+
+    print(board)
+
+    

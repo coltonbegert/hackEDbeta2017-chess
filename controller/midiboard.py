f5ae1651934800d4d75938feda1cc5492a5bd19c
Steven Boddez <stevenboddez@gmail.com>
2017-11-18 23:00:26 -0700

    Can push arbitrary colours to the Midi Fighter 64

---
+++ controller/midiboard.py
@@ -0,0 +1,9 @@
+class MIDIBoard():
+    def __init__(self):
+        self._board = [[['00', '00', '00'] for i in range(8)] for i in range(8)]
+
+    def set(self, pos, value):
+        self._board[pos[0]][pos[1]] = value
+
+    def get(self, pos):
+        return self._board[pos[0]][pos[1]]

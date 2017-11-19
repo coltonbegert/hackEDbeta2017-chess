c41ce8c5139392296828eb5c45d2c0d534041f06
Ryan Huard <ryan.huard@live.ca>
2017-11-19 01:05:36 -0700

    Make faster

--- PGNParser/testdriver.py
+++ PGNParser/testdriver.py
@@ -5,10 +5,18 @@ gameSet = PGNParser("c:\\Users\\RyanH\Development\\hackEDbeta2017-chess\\Test Da
 write_file = "datums.csv"
 with open(write_file, "w") as output:
     try:
+        lineCount = 0
+        lines = []
         while(True):
             datum = gameSet.parse_next_game()
             if not datum is None:
-                output.write(repr(datum) + "\n")
+                lineCount += 1
+                lines.append(repr(datum))
+                if(lineCount >= 1000):
+                    output.write("\n".join(lines))
+                    lineCount = 0
+                    lines = []
     except ValueError:
+        output.write("\n".join(lines))
         print("Done maybe")
         
